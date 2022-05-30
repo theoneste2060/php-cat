b693d141071b00e388e20e2a88f21efc1a269bb1
Theoneste <theoneste2060.com>
2022-05-30 08:38:02 -0700

    finished dashboard page

---
+++ dashboard.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.0.2-dist/css/bootstrap.rtl.min.css">
+    <title>Document</title>
+    
+</head>
+<body>
+    <div class="container">
+        <section>
+            <div class="row bg-primary text-white pb-1 pt-1"><center>IREMBO | Practical CAT</center></div>
+            <div class="row" style="height:20vh"></div>
+            <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4 card pt-5" >
+                
+                <form method="post">
+                    <div class="row">
+                        <center>
+                            <img src="Picture1.png" class="pb-5" alt="">
+                        </center>
+                    </div>
+                    <div class="row">
+                        <center>
+                            <h3>TUYISHIMIRE Peter</h3>
+                            <span>tuyishimirepeter@gmail.com</span>
+                        </center>
+                    </div>
+
+                    <div class="form-group pt-3 pb-5">
+                      <center> <input type="submit"name ="btn" value="Logout" 
+                      class="btn bg-primary text-white"></center>
+                      
+                    </div>
+                    <div class="row col-sm-12" style="height:35vh;"></div>
+                </form>
+            </div>
+            <div class="col-md-4"></div>
+            </div>
+            
+        </section>
+    </div>
+</body>
+</html>
